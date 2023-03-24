ed49573504961c9a3bc84499cbb415dab9ec9c26
Wiibleyde <nathan@bonnell.fr>
2023-03-24 10:04:52 +0100

    update
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- declarations.js
+++ declarations.js
@@ -4,13 +4,13 @@ const obj = Object.freeze({
     str: "some string",
     num: 42,
     bool: true,
-    undef: undefined,
-    nested: Object.freeze({
-        arr: Object.freeze([4, undefined, '2']),
-        obj: Object.freeze({
-            str: "another string",
-            num: 3.14,
-            bool: false
-        })
+    undef: undefined
+});
+const nested = Object.freeze({
+    arr: Object.freeze([4, undefined, '2']),
+    obj: Object.freeze({
+        str: "another string",
+        num: 3.14,
+        bool: false
     })
 });
