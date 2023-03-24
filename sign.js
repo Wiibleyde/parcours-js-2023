a4ae218b3254f93a7014f16f1f062b47d1517441
Wiibleyde <nathan@bonnell.fr>
2023-03-24 11:25:35 +0100

    sign
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- sign.js
+++ sign.js
@@ -1,10 +1,11 @@
 function sign(number) {
     switch (number) {
-        case 0:
-            return 0;
-        case number > 0:
-            return 1;
-        case number < 0:
-            return -1;
+        case 0:return 0;
+        case number > 0:return 1;
+        case number < 0:return -1;
     }
+}
+
+function sameSign(number1,number2) {
+    return sign(number1) === sign(number2);
 }
