b03d4701f3940460c472feae8a2f4e04381f1ac8
Wiibleyde <nathan@bonnell.fr>
2023-03-24 09:41:47 +0100

    declaration
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ declarations.js
@@ -0,0 +1,7 @@
+const escapeStr = "Oui`\/'"+"'"
+const arr = [4,'2']
+const obj = {"str":"Oui","num":1,"bool":true,"undef":undefined}
+const nested = {
+    "arr":[4,undefined,'2'],
+    "obj":{"str":"Oui","num":1,"bool":true,"undef":undefined}
+}
