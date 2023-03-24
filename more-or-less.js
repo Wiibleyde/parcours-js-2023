f0a41a07b60c2e8999cd0f62ca4737a2244db300
Wiibleyde <nathan@bonnell.fr>
2023-03-24 10:15:57 +0100

    more or less
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ more-or-less.js
@@ -0,0 +1,15 @@
+function more(number) {
+    return number+1
+}
+
+function less(number) {
+    return number-1
+}
+
+function add(number1,number2) {
+    return number1+number2
+}
+
+function sub(number1,number2) {
+    return number1-number2
+}
