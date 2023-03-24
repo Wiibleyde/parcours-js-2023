76b0ad2ecd3a3440607e9c15fb29cae7c649f346
Wiibleyde <nathan@bonnell.fr>
2023-03-24 11:35:00 +0100

    is update
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- is.js
+++ is.js
@@ -1,12 +1,15 @@
-is.num = (value) => typeof value === 'number' && !isNaN(value)
-is.nan= (value) => Number.isNaN(value)
-is.str= (value) => typeof value === 'string'
-is.bool= (value) => typeof value === 'boolean'
-is.undef= (value) => typeof value === 'undefined'
-is.def= (value) => typeof value !== 'undefined'
-is.arr= (value) => Array.isArray(value)
-is.obj= (value) => value !== null && typeof value === 'object' && !Array.isArray(value)
-is.fun= (value) => typeof value === 'function'
-is.truthy= (value) => Boolean(value)
-is.falsy= (value) => !Boolean(value)
+const is = {
+    ...is,
+    num: (value) => typeof value === 'number' && !isNaN(value),
+    nan: (value) => Number.isNaN(value),
+    str: (value) => typeof value === 'string',
+    bool: (value) => typeof value === 'boolean',
+    undef: (value) => typeof value === 'undefined',
+    def: (value) => typeof value !== 'undefined',
+    arr: (value) => Array.isArray(value),
+    obj: (value) => value !== null && typeof value === 'object' && !Array.isArray(value),
+    fun: (value) => typeof value === 'function',
+    truthy: (value) => Boolean(value),
+    falsy: (value) => !Boolean(value),
+};
   
