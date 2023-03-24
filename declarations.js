a50ac8f45019195b243c7ea3de951ce25a9a90c6
Wiibleyde <nathan@bonnell.fr>
2023-03-24 10:00:25 +0100

    Freeze ?
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- declarations.js
+++ declarations.js
@@ -1,8 +1,4 @@
 const escapeStr = "Oui`\\//'"+'"'
 const arr = [4,'2']
 const obj = {"str":"Oui","num":1,"bool":true,"undef":undefined}
-const nested = {
-    "arr":[4,undefined,'2'],
-    "obj":{"str":"Oui","num":1,"bool":true,"undef":undefined}
-}
-Object.freeze(nested)
+Object.freeze(nested = {"arr":[4,undefined,'2'],"obj":{"str":"Oui","num":1,"bool":true,"undef":undefined}})
