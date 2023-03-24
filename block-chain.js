c3bb269f13a896bdea77c3f3bf35b600e5c09a38
Wiibleyde <nathan@bonnell.fr>
2023-03-24 12:27:23 +0100

    block chain update
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- block-chain.js
+++ block-chain.js
@@ -1,12 +1,9 @@
 const blockChain = (data, prev = { index: 0, hash: '0' }) => {
     const index = prev.index + 1;
     const hash = hashCode(`${index}${prev.hash}${JSON.stringify(data)}`);
-    return {
-        index,
-        hash,
-        data,
-        prev,
-        chain: (newData) => blockChain(newData, { index, hash }),
-    };
-};
+    const block = { index, hash, data, prev };
   
+    block.chain = (data) => blockChain(data, block);
+  
+    return block;
+};
