3c26d197a5e753984e730543a417086df452e42f
Wiibleyde <nathan@bonnell.fr>
2023-03-24 10:10:27 +0100

    clone
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ mutability.js
@@ -0,0 +1,16 @@
+const person = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+}
+
+const clone1 = { ...person }
+const clone2 = { ...person }
+const samePerson = person
+
+person.age = person.age + 1
+person.country = 'FR'
+
+console.log(clone1)
+console.log(clone2)
+console.log(samePerson)
