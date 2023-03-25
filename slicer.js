13334ca6cb49232b1f024320e3c3b49a0ef711e9
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-03-25 10:27:30 +0100

    slicer
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- slicer.js
+++ slicer.js
@@ -1,7 +1,9 @@
-function slice(element, start, end) {
+function slice(element, start, end=element.length) {
     var result = "";
     for (var i = start; i < end; i++) {
         result += element[i];
     }
     return result;
 }
+
+console.log(slice('abcdef', 2));
