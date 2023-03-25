269796bd7410e178113141e2a914617a8e9e4ce2
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-03-25 10:25:54 +0100

    index-of
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

---
+++ index-of.js
@@ -0,0 +1,26 @@
+function indexOf(arr, value, startIndex = 0) {
+    for (let i = startIndex; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function lastIndexOf(arr, value, startIndex = arr.length - 1) {
+    for (let i = startIndex; i >= 0; i--) {
+        if (arr[i] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function includes(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return true;
+        }
+    }
+    return false;
+}
