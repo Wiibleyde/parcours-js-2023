128e1a8c856b7b1ce103e1a4493c68da3394373f
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-03-25 10:18:24 +0100

    elementary
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- elementary.js
+++ elementary.js
@@ -1,23 +1,65 @@
 function multiply(a, b) {
     let result = 0;
+    let negative = false;
+    if (b < 0 || a < 0) {
+        negative = true;
+        
+        if (b < 0) {
+            b = -b;
+        }
+        if (a < 0) {
+            a = -a;
+        }
+    } 
     for (let i = 0; i < b; i++) {
         result += a;
+    }
+    if (negative) {
+        result = -result;
     }
     return result;
 }
 
 function divide(a, b) {
     let result = 0;
+    let negative = false;
+    if (b < 0 || a < 0) {
+        negative = true;
+        if (b < 0) {
+            b = -b;
+        }
+        if (a < 0) {
+            a = -a;
+        }
+    }
     while (a >= b) {
         a -= b;
         result++;
+    }
+    if (negative) {
+        result = -result;
     }
     return result;
 }
 
 function modulo (a, b) {
+    let negative = false;
+    if (b < 0 || a < 0) {
+        negative = true;
+        if (b < 0) {
+            b = -b;
+        }
+        if (a < 0) {
+            a = -a;
+        }
+    }
     while (a >= b) {
         a -= b;
     }
+    if (negative) {
+        a = -a;
+    }
     return a;
 }
+
+console.log(multiply(123, -22));
