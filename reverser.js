c54c3bbb579a9e5198dcba27dfed97b1fbe64055
Wiibleyde <nathan@bonnell.fr>
2023-03-27 12:00:00 +0200

    reverse
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ reverser.js
@@ -0,0 +1,7 @@
+function reverse(arrayVar) {
+    var reversedArray = [];
+    for (var i = arrayVar.length - 1; i >= 0; i--) {
+        reversedArray.push(arrayVar[i]);
+    }
+    return reversedArray;
+}
