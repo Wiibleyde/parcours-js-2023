c841efa07ad6f1603fa4c5e702c12ff6a5acea8a
Wiibleyde <nathan@bonnell.fr>
2023-03-27 13:32:30 +0200

    get
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- get.js
+++ get.js
@@ -1,8 +1,8 @@
 function get(src,path) {
+    if (src === undefined || path === undefined) return undefined;
     var path = path.split('.');
     for (var i = 0; i < path.length; i++) {
         src = src[path[i]];
     }
     return src;
 }
-
