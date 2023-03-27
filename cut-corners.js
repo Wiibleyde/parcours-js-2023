f2c3cda0fe0f9a0cd389654a80ae3a17feb69eb7
Wiibleyde <nathan@bonnell.fr>
2023-03-27 11:10:34 +0200

    cut corners
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- cut-corners.js
+++ cut-corners.js
@@ -1,37 +1,72 @@
-function round(number) {
-    let integerPart = number >= 0 ? ~~number : ceil(number);
-    let decimalPart = Math.abs(number - integerPart);
-    
-    if (decimalPart >= 0.5) {
-        return integerPart + Math.sign(number);
-    } else {
-        return integerPart;
-    }
-}
+function round(n) {
+    let ost
+    let res
+    let flag = false
 
-function ceil(number) {
-    let integerPart = ~~number;
-    
-    if (integerPart === number) {
-        return integerPart;
-    } else if (number > 0) {
-        return integerPart + 1;
-    } else {
-        return integerPart;
+    if (n < 0) {
+        n = -n
+        flag = true
     }
+
+    ost = n % 1
+
+    if (ost > 0.5) {
+        res = n + 1 - ost
+    } else {
+        res = n - ost
+    }
+
+    if (flag) {
+        res = -res
+    }
+
+    return res
+}
+function ceil(n) {
+    let ost
+    let res
+    ost = n % 1
+
+    if (n > 0 && ost != 0) {
+        return res = n + 1 - ost
+    } else {
+        return res = n - ost
+    }
+
+    return n
+}
+function floor(n) {
+    let ost
+    let res
+    ost = n % 1
+    
+    if (n > 0 && ost != 0) {
+        return res = n - ost
+    } else if (ost === 0) {
+        return n
+    } else {
+        let x
+        x = 1 + ost
+        return res = n - x
+    }
+    
+    return n
+}
+function trunc(n) {
+    let ost
+    let res
+    ost = n % 1
+    
+    if (n > 0 && ost != 0) {
+        return res = n - ost
+    } else {
+        let x
+        x = 1 + ost
+        return res = n - ost
+    }
+    
+    return n
 }
   
-function floor(number) {
-    let integerPart = ~~number;
-    return integerPart;
-}
-  
-function trunc(number) {
-    if (number >= 0) {
-        return ~~number;
-    } else {
-        return ~~(-number);
-    }
-}
 
-console.log(round(1.1))
+console.log(round(1.6));
