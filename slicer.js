f0f774c0341970274e53a12ddd4b84ff340c621b
Wiibleyde <nathan@bonnell.fr>
2023-03-27 09:36:43 +0200

    slicer js
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- slicer.js
+++ slicer.js
@@ -1,15 +1,30 @@
 function slice(element, start, end=element.length) {
-    var result = "";
-    if (start < 0) {
-        start = element.length + start;
+    if (typeof element === "object") {
+        let result = [];
+        if (start < 0) {
+            start = element.length + start;
+        }
+        if (end < 0) {
+            end = element.length + end;
+        }
+        for (let i = start; i < end; i++) {
+            result.push(element[i]);
+        }
+        return result;
+    } else if (typeof element === "string") {
+        var result = "";
+        if (start < 0) {
+            start = element.length + start;
+        }
+        if (end < 0) {
+            end = element.length + end;
+        }
+        for (var i = start; i < end; i++) {
+            result += element[i];
+        }
+        return result;
     }
-    if (end < 0) {
-        end = element.length + end;
-    }
-    for (var i = start; i < end; i++) {
-        result += element[i];
-    }
-    return result;
+    return [];
 }
 
-console.log(slice('abcdef', 0, -2));
+console.log(slice([1, 2, 3, 4, 5, 6], 2));
