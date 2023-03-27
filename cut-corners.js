9e35f689376a651c26014f81925becf1be767f96
Wiibleyde <nathan@bonnell.fr>
2023-03-27 11:14:15 +0200

    cut remove %
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- cut-corners.js
+++ cut-corners.js
@@ -1,5 +1,6 @@
 function round(n) {
-    let ost
+
+    let mod
     let res
     let flag = false
 
@@ -8,12 +9,12 @@ function round(n) {
         flag = true
     }
 
-    ost = n % 1
+    mod = modulo(n%1)
 
-    if (ost > 0.5) {
-        res = n + 1 - ost
+    if (mod > 0.5) {
+        res = n + 1 - mod
     } else {
-        res = n - ost
+        res = n - mod
     }
 
     if (flag) {
@@ -22,51 +23,78 @@ function round(n) {
 
     return res
 }
-function ceil(n) {
-    let ost
-    let res
-    ost = n % 1
 
-    if (n > 0 && ost != 0) {
-        return res = n + 1 - ost
+function ceil(n) {
+
+    let mod
+    let res
+    mod = modulo(n%1)
+
+    if (n > 0 && mod != 0) {
+        return res = n + 1 - mod
     } else {
-        return res = n - ost
+        return res = n - mod
     }
 
     return n
 }
+
 function floor(n) {
-    let ost
+
+    let mod
     let res
-    ost = n % 1
+    mod = modulo(n%1)
     
-    if (n > 0 && ost != 0) {
-        return res = n - ost
-    } else if (ost === 0) {
+    if (n > 0 && mod != 0) {
+        return res = n - mod
+    } else if (mod === 0) {
         return n
     } else {
         let x
-        x = 1 + ost
+        x = 1 + mod
         return res = n - x
     }
     
     return n
 }
+
 function trunc(n) {
-    let ost
+
+    let mod
     let res
-    ost = n % 1
+    mod = modulo(n%1)
     
-    if (n > 0 && ost != 0) {
-        return res = n - ost
+    if (n > 0 && mod != 0) {
+        return res = n - mod
     } else {
         let x
-        x = 1 + ost
-        return res = n - ost
+        x = 1 + mod
+        return res = n - mod
     }
     
     return n
 }
-  
+
+function modulo (a, b) {
+
+    let negative = false
+    
+    if (b < 0 || a < 0) {
+        if (b < 0) {
+            b = -b;
+        }
+        if (a < 0) {
+            negative = true;
+            a = -a;
+        }
+    }
+    while (a >= b) {
+        a -= b;
+    }
+    if (negative) {
+        a = -a;
+    }
+    return a;
+}
 
 console.log(round(1.6));
