cb6294b063913142cba6726491398275650e8791
Wiibleyde <nathan@bonnell.fr>
2023-03-27 13:42:53 +0200

    triangle
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- triangle.js
+++ triangle.js
@@ -1,10 +1,12 @@
-function triangle(size) {
+function triangle(string,size) {
     var result = "";
     for (var i = 0; i < size; i++) {
         for (var j = 0; j <= i; j++) {
-        result += "*";
+        result += string;
         }
         result += "\n";
     }
     return result;
 }
+
+console.log(triangle("ඞ", 5));
