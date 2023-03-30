f5dd3facdbea3d96ce87f5abd561f412d3ba635d
Wiibleyde <nathan@bonnell.fr>
2023-03-30 09:50:55 +0200

    count leap year
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

---
+++ count-leap-years.js
@@ -0,0 +1,12 @@
+function countLeapYears(date) {
+    let year = date.getFullYear()
+    let count = 0
+    for (let i = 1; i < year; i++) {
+        if (i % 4 === 0 && i % 100 !== 0 || i % 400 === 0) {
+            count++
+        }
+    }
+    return count
+}
+
+console.log(countLeapYears(new Date('2023-03-30')))
