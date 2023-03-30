40464d80ecbabca3193f83f5cd49291f1af8fbfd
Wiibleyde <nathan@bonnell.fr>
2023-03-30 10:17:51 +0200

    sunny sunday
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,11 +1,9 @@
-function sunnySunday(date) {
-    let weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    let day = date.getDay()
-    if (day === 0) {
-        return weekdays[day]
-    } else {
-        return weekdays[day - 1]
-    }
+function getWeekday(date) {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const timeDiff = date.getTime() - new Date('0001-01-01').getTime()
+    const dayDiff = Math.floor(timeDiff / (1000 * 3600 * 24)) + 1
+    const weekdayIndex = (dayDiff - 1) % 6
+    return days[weekdayIndex]
 }
 
-console.log(sunnySunday(new Date('0001-01-07')))
+console.log(getWeekday(new Date('2020-05-30')))
