e4e74b4504b2defd485eba5e8c1ca17e4d51e317
Wiibleyde <nathan@bonnell.fr>
2023-03-30 09:24:00 +0200

    rebecca
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- rebecca-black.js
+++ rebecca-black.js
@@ -7,9 +7,11 @@ function isWeekend(date) {
 }
 
 function isLeapYear(year) {
-    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+    return new Date(year, 1, 29).getMonth() === 1;
 }
 
 function isLastDayOfMonth(date) {
     return date.getDate() === new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 }
+
+console.log(isLeapYear(new Date('1993-02-01')));
