8163f1b92c0c843df4367ee79f7ee6800bfea51a
Wiibleyde <nathan@bonnell.fr>
2023-04-04 15:33:15 +0200

    debounce
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- debounce.js
+++ debounce.js
@@ -8,30 +8,35 @@ function debounce(func, waitTime) {
     }
 }
 
-function opDebounce(func, waitTime, leading) {
+function opDebounce(func, waitTime, options) {
     let timer = null
     let first = true
+    let leading
+    if (typeof options === 'object') {
+        leading = !!options.leading
+    }
     return function () {
-        let context = this
-        let args = arguments
+        let context = this,
+        args = arguments
         if (first && leading) {
             func.apply(context, args)
             first = false
         }
         if (timer) {
-            return
+            clearTimeout(timer)
         }
-        timer = setTimeout(function () {func.apply(context, args),timer = null}, waitTime)
+        timer = setTimeout(function () {func.apply(context, args)}, waitTime)
     }
 }
 
+
 const debounceFunc = debounce(() => {
     console.log('debounce')
-}, 1000)
+}, 500)
 
 const opDebounceFunc = opDebounce(() => {
     console.log('opDebounce')
-}, 1000, true)
+}, 1000, {leading: true})
 
 debounceFunc()
 opDebounceFunc()
